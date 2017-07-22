42a093b08d45cc8b94f585165d3f3d6a41d6355c
shravankt <tiwari_shravan@yahoo.com>
2017-07-22 17:32:26 +0200

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -4,21 +4,23 @@ game = [[1,2,1],
 
 def winner(list):
     for i in range(3):
-        if game[i][0] == game[i][1] == game[i][2] == 1:
-            print "Player 1 is the winner"
-        elif game[0][i] == game[1][i] == game[2][i] == 1:
-            print "Player 1 is the winner"
-        if game[i][0] == game[i][1] == game[i][2] == 2:
-            print "Player 2 is the winner"
-        elif game[0][i] == game[1][i] == game[2][i] == 2:
-            print "Player 2 is the winner"
-        if game[0][0] == game[1][1] == game[2][2] == 1:
-            print "Player 1 is the winner"
-        elif game[0][0] == game[1][1] == game[2][2] == 2:
-            print "Player 2 is the winner"
-        if game[2][0] == game[1][1] == game[0][2] == 1:
-            print "Player 1 is the winner"
-        elif game[2][0] == game[1][1] == game[0][2] == 2:
-            print "Player 2 is the winner" 
+        if list[i][0] == list[i][1] == list[i][2] == 1:
+            return ('Winner is player:' +str(1))
+        elif list[0][i] == list[1][i] == list[2][i] == 1:
+            return ('Winner is player:' +str(1))
+        if list[i][0] == list[i][1] == list[i][2] == 2:
+            return ('Winner is player:' +str(2))
+        elif list[0][i] == list[1][i] == list[2][i] == 2:
+            return ('Winner is player:' +str(2))
+        if list[0][0] == list[1][1] == list[2][2] == 1:
+            return ('Winner is player:' +str(1))
+        elif list[0][0] == list[1][1] == list[2][2] == 2:
+            return ('Winner is player:' +str(2))
+        if list[2][0] == list[1][1] == list[0][2] == 1:
+            return ('Winner is player:' +str(1))
+        elif list[2][0] == list[1][1] == list[0][2] == 2:
+            return ('Winner is player:' +str(2))
+        #else:
+         #   return ("it's a tie")
 
 print winner(game)
